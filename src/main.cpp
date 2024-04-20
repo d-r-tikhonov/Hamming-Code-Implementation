b5a5162f5be6e743160690f356fbaa015c1a1362
Dmitry Tikhonov <tikhonov.dr@phystech.edu>
2024-04-20 03:56:57 +0300

    Edited example

--- src/main.cpp
+++ src/main.cpp
@@ -39,10 +39,14 @@ int main()
     std::string decoded_noise_message = hamming_code::decode(std::stoul(encoded_noise_message, 0, 2), encoded_noise_message.length());
     std::cout << "Decoded noise message: 0x" << std::hex << std::stoul(decoded_noise_message, 0, 2) << std::endl;
 
+    uint32_t input_bin_message = 0b1101001100110101;
+    encoded_message = hamming_code::encode(input_message, n_bits);
+    std::cout << "\nEncoded message (0b" << utils::int2bin(input_bin_message, n_bits) << "): 0b" << encoded_message << std::endl;
+
     n_bits = 12;
     uint32_t message = 0xE4F;
     decoded_message = hamming_code::decode(message, n_bits);
-    std::cout << "\nDecoded message: 0x" << std::hex << std::stoul(decoded_message, 0, 2) << std::endl;
+    std::cout << "\nDecoded message (0x" << std::hex << message << "): 0x" << std::hex << std::stoul(decoded_message, 0, 2) << std::endl;
 
     return 0;
 }
